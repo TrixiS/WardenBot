4f615ec3c16061332a6eb79c052a6694ddcb09b0
TrixiS <oficialmorozov@gmail.com>
2019-12-28 11:15:14 +0500

    Added main

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,12 @@
 import logging
 
 from bot import Warden
+from cogs.utils.config import Config
 
 
-logging.basicConfig(level=logging.DEBUG, format=f"%(levelname)s: %(message)s - %(asctime)s")
+logging.basicConfig(level=logging.INFO, format=f"%(levelname)s: %(message)s - %(asctime)s")
+
+config = Config()
+
+bot = Warden(command_prefix=config.prefixes, config=config)
+bot.run(config.bot_token, reconnect=True)
