135216943c860b679bf9e52fae7c37df6bd60e8e
TrixiS <oficialmorozov@gmail.com>
2019-12-29 14:51:38 +0500

    Added tag check command and Index converters

--- src/cogs/tags.py
+++ src/cogs/tags.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from typing import Optional
 from math import ceil
 
-from .utils.converters import MemberOrAuthor, Index
+from .utils.converters import IndexConverter, Index
 from .utils.strings import markdown
 
 
@@ -14,26 +14,27 @@ class TagCog(commands.Cog):
         self.bot = bot
 
         self.tag_check_page_max = 30
-        self.tag_name_max_len = 60
 
     @commands.group(invoke_without_command=True, aliases=['t'])
     async def tag(self, ctx, *, name: commands.clean_content):
-        check = await self.bot.db.execute("SELECT `content` FROM `tags` WHERE `tags`.`member` = ? AND `tags`.`name` = ?",
+        check = await self.bot.db.execute("SELECT `content` FROM `tags` WHERE `tags`.`member` = ? AND `tags`.`name` LIKE ?",
             ctx.author.id, name)
 
         if check is not None:
             await ctx.send(check)
             await ctx.message.delete()
 
-            await self.bot.db.execute("UPDATE `tags` SET `used` = `used` + 1 WHERE `tags`.`member` = ? AND `tags`.`name` = ?",
+            await self.bot.db.execute("UPDATE `tags` SET `used` = `used` + 1 WHERE `tags`.`member` = ? AND `tags`.`name` LIKE ?",
                 ctx.author.id, name, with_commit=True)
         else:
             await ctx.answer(ctx.lang["tags"]["no"].format(name))
 
-    @tag.command(name='check')
-    async def tag_check(self, ctx, member: MemberOrAuthor, page: Index=0):
-        check = await self.bot.db.execute("SELECT `name` FROM `tags` WHERE `tags`.`member` = ? ORDER BY `tags`.`created` OFFSET ? LIMIT ?",
-            member.id, self.tag_check_page_max * page, self.tag_check_page_max,
+    @tag.command(name="check")
+    async def tag_check(self, ctx, member: Optional[discord.Member]=None, page: IndexConverter=Index(0)):
+        member = member or ctx.message.author
+    
+        check = await self.bot.db.execute("SELECT `name` FROM `tags` WHERE `tags`.`member` = ? ORDER BY `tags`.`created` LIMIT ? OFFSET ?",
+            member.id, self.tag_check_page_max, self.tag_check_page_max * page.value,
             fetch_all=True
         )
 
@@ -46,13 +47,29 @@ class TagCog(commands.Cog):
                 colour=ctx.color
             )
             em.set_thumbnail(url=member.avatar_url)
-            em.set_footer(text=f'{ctx.lang["shared"]["page"]}: {page}/{ceil(count / self.tag_check_page_max)}')           
+            em.set_footer(text=f'{ctx.lang["shared"]["page"]}: {page.humanize()}/{ceil(count / self.tag_check_page_max)}')           
 
             return await ctx.send(embed=em)
 
-        await ctx.answer(ctx.lang["tags"]["dont_have_any"].format(member.mention))            
+        await ctx.answer(ctx.lang["tags"]["dont_have_any"].format(member.mention, page.humanize()))            
 
+    @tag.command(name="create")
+    async def tag_create(self, ctx, *, name: commands.clean_content):
+        check = await self.bot.db.execute("SELECT `name` FROM `tags` WHERE `tags`.`member` = ? AND `tags`.`name` LIKE ?",
+            ctx.message.author.id, name)
 
+        if check is None:
+            content = await commands.clean_content().convert(ctx,
+                await ctx.ask(ctx.lang["tags"]["content?"], timeout=30.0)
+            )
+
+            await self.bot.db.execute("INSERT INTO `tags` VALUES (?, ?, ?, ?, UNIX_TIMESTAMP())",
+                ctx.message.author.id, name, content, 0, with_commit=True)
+
+            return await ctx.answer(ctx.lang["tags"]["created"].format(name))
+
+        await ctx.answer(ctx.lang["tags"]["already_created"].format(name))
+            
 
 def setup(bot):
     bot.add_cog(TagCog(bot))
--- src/cogs/utils/converters.py
+++ src/cogs/utils/converters.py
@@ -2,36 +2,36 @@ import discord
 
 from discord.ext import commands
 
-
-class MemberOrAuthor(commands.MemberConverter):
-
-    async def convert(self, ctx, arg):
-        try:
-            return await super().convert(ctx, arg)
-        except:
-            return ctx.author
-
-
 class Uint(commands.Converter):
 
-    def __init__(self, include_zero=False):
+    def __init__(self, include_zero=True):
         self.include_zero = include_zero
 
     async def convert(self, ctx, arg):
         arg = int(arg)
 
         if arg < 0:
-            raise commands.BadArgument("Argument must be >= 0")
+            raise commands.BadArgument(ctx.lang["errors"]["arg_over_or_equal_zero"])
 
         if arg == 0 and not self.include_zero:
-            raise commands.BadArgument("Argument must be > 0")
+            raise commands.BadArgument(ctx.lang["errors"]["arg_over_zero"])
 
         return arg
 
 
-class Index(Uint):
+class Index:
+
+    def __init__(self, value: int):
+        self.value = value
+
+    def humanize(self) -> int:
+        return self.value + 1
+
+
+class IndexConverter(Uint):
 
     async def convert(self, ctx, arg):
         convertered = await super().convert(ctx, arg) - 1
 
-        return convertered if convertered >= 0 else 0
+        return Index(convertered if convertered >= 0 else 0)
+        
