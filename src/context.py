02161d7879c98f24e7276c3c7fcbd20d2caf0343
TrixiS <oficialmorozov@gmail.com>
2019-12-29 13:54:58 +0500

    Added ask method for context class

--- src/context.py
+++ src/context.py
@@ -1,6 +1,8 @@
 from discord.ext.commands import Context
 from discord import Embed
 
+from asyncio import TimeoutError
+
 
 class WardenContext(Context):
     
@@ -15,3 +17,15 @@ class WardenContext(Context):
         em.set_author(name=self.message.author.name, icon_url=self.message.author.avatar_url)
 
         await self.send(embed=em)
+
+    async def ask(self, text: str, **kwargs) -> str:
+        await self.send(f"{self.message.author.mention}, {text}")
+
+        content = ""
+
+        try:
+            check = kwargs.pop("check", lambda m: m.author == self.author)
+
+            content = (await self.bot.wait_for("message", **kwargs, check=check)).content
+        finally:
+            return content
