2cc36fd874cc91a381662248b3a71959bc99074d
TrixiS <oficialmorozov@gmail.com>
2019-12-31 15:01:56 +0500

    Added shared constants

---
+++ src/cogs/utils/constants.py
@@ -0,0 +1,22 @@
+
+
+
+class RanksConstants:
+
+    ROLE_NAME_MAX_LEN = 80
+
+
+class EmbedConstants:
+
+    TITLE_MAX_LEN = 256
+    DESC_MAX_LEN = 2048
+    FIELD_MAX_COUNT = 25
+    FIELD_NAME_MAX_LEN = 256
+    FIELD_VALUE_MAX_LEN = 1024
+    FOOTER_TEXT_MAX_LEN = 2048
+    AUTHOR_NAME_MAX_LEN = 256
+
+
+class StringConstants:
+
+    DOT_SYMBOL = '•'
