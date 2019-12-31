b41c7ef6dd6b85fa89682e30f7447fdf15e3fa40
TrixiS <oficialmorozov@gmail.com>
2019-12-31 15:02:56 +0500

    Added ranks toggle command

--- src/cogs/ranks.py
+++ src/cogs/ranks.py
@@ -47,6 +47,57 @@ class RankCog(commands.Cog):
 
             await ctx.send(embed=em)
 
+    @rank.command(name="toggle")
+    async def rank_toggle(self, ctx, roles: commands.Greedy[discord.Role]):
+        roles = {role for role in roles if role < ctx.guild.me.top_role}
+
+        if not len(roles):
+            raise commands.BadArgument(ctx.lang["errors"]["no_roles"])
+        
+        check = await self.bot.db.execute("SELECT `role` FROM `ranks` WHERE `ranks`.`server` = ?",
+            ctx.guild.id, fetch_all=True)
+
+        ranks_roles = tuple(ctx.guild.get_role(c[0]) for c in check)
+
+        del check
+
+        deleted = []
+        added = []
+
+        for role in roles:
+            if role in ranks_roles:
+                await self.bot.db.execute("DELETE FROM `ranks` WHERE `ranks`.`server` = ? AND `ranks`.`role` = ?",
+                    ctx.guild.id, role.id, with_commit=True)
+                
+                deleted.append(role)
+            else:
+                await self.bot.db.execute("INSERT INTO `ranks` VALUES (?, ?)",
+                    ctx.guild.id, role.id, with_commit=True)
+
+                added.append(role)
+
+        def build_field(ranks: list) -> str:
+            result = ', '.join(
+                role.mention for role in ranks 
+                if role is not None
+            )
+
+            return result or ctx.lang["shared"]["no"]
+
+        em = discord.Embed(colour=ctx.color, title=ctx.lang["ranks"]["title"])
+        em.add_field(
+            name=ctx.lang["shared"]["added"], 
+            value=build_field(added), 
+            inline=False
+        )
+        em.add_field(
+            name=ctx.lang["shared"]["deleted"],
+            value=build_field(deleted),
+            inline=False
+        )
+
+        await ctx.send(embed=em)
+
 
 def setup(bot):
     bot.add_cog(RankCog(bot))
