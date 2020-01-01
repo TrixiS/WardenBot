79c0c502b48cad8109b99e6ae65d45751a0edd6d
TrixiS <oficialmorozov@gmail.com>
2020-01-01 16:05:16 +0500

    Added `join_or_default` and `collect_attributes` functions

--- src/cogs/utils/strings.py
+++ src/cogs/utils/strings.py
@@ -9,4 +9,19 @@ def multi_replace(string: str, tokens: list, to_replace: str) -> str:
 
 def markdown(string: str, tag: str) -> str:
     return f"{tag}{string}{tag}"
-    
+
+
+def join_or_default(iterable, separator: str, default: str="") -> str:
+    result = separator.join(iterable)
+
+    return result or default
+
+
+def collect_attributes(iterable, *args) -> str:
+    result = []
+
+    for item in iterable:
+        result.append(str(getattr(item, *args)))
+
+    return result
+
