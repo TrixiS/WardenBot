53f6a586d922d60a3c88ce6cf5e2f4af2ef22ea4
TrixiS <oficialmorozov@gmail.com>
2020-01-07 18:39:44 +0500

    Adapted UnixTime class for datetime.timedelta

--- src/cogs/utils/time.py
+++ src/cogs/utils/time.py
@@ -1,9 +1,19 @@
 import datetime
+import time
+
 
 class UnixTime:
 
-    def __init__(self, value: int):
+    def __init__(self, value):
         self.value = value
+
+    @classmethod
+    def now(cls):
+        return cls(time.time())
+
+    @property
+    def timestamp(self):
+        return self.value
 
     def to_datetime(self):
         return datetime.datetime.fromtimestamp(self.value)
@@ -11,3 +21,14 @@ class UnixTime:
     def humanize(self, fmt="%d.%m.%Y"):
         return self.to_datetime().strftime(fmt)
 
+    def __add__(self, timedelta: datetime.timedelta):
+        now = self.now()
+        now.value += timedelta.total_seconds()
+        return now
+
+    def __sub__(self, timedelta: datetime.timedelta):
+        now = self.now()
+        now.value -= timedelta.total_seconds()
+        return now
+
+        
