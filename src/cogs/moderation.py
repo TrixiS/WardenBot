18959d67816797782b9845a4d2ac5b9930a497f8
TrixiS <oficialmorozov@gmail.com>
2020-01-09 18:15:19 +0500

    MutePool refactored

--- src/cogs/moderation.py
+++ src/cogs/moderation.py
@@ -19,19 +19,44 @@ MuteInfo = namedtuple("MuteInfo", ["time", "reason"])
 
 
 class MuteRoles:
-
+    
+    overwrite = discord.PermissionOverwrite()
+    overwrite.send_messages = False
+    overwrite.embed_links = False
+    overwrite.attach_files = False
+    overwrite.create_instant_invite = False
+    overwrite.manage_messages = False
+    overwrite.send_tts_messages = False
+    overwrite.mention_everyone = False
+    overwrite.speak = False
+    overwrite.use_voice_activation = False
+    
     def __init__(self, bot):
         self.bot = bot
 
-    async def setup_mute_role(self, role=None):
-        raise NotImplementedError() from None
+    async def setup_mute_role(self, *, guild=None, role=None):
+        if role is None:
+            role = await guild.create_role(name="Muted")
+
+        for channel in role.guild.channels:
+            await channel.set_permissions(role, overwrite=self.overwrite)
+
+        check = await self.bot.db.execute("UPDATE `mute_roles` SET `role` = ? WHERE `mute_roles`.`server` = ?",
+            role.id, role.guild.id, with_commit=True)
+
+        if not check:
+            await self.bot.db.execute("INSERT INTO `mute_roles` VALUES (?, ?)",
+                role.guild.id, role.id, with_commit=True)
+
+        return role
 
     async def get_mute_role(self, guild: discord.Guild):
         role_id = await self.bot.db.execute("SELECT `role` FROM `mute_roles` WHERE `mute_roles`.`server` = ?",
             guild.id)
 
-        role = guild.get_role(role_id) or await self.setup_mute_role()
-        return roe
+        role = guild.get_role(role_id) or await self.setup_mute_role(guild=guild)
+        
+        return role
 
 # TODO:
 #   add langs for all methods below
@@ -50,39 +75,23 @@ class MutePool:
         return self._create_pair(guild, member) in self.pool
 
     async def mute_task(self, guild, member, time: int):
-        if time <= 0:
-            await asyncio.sleep(time)
-        
+        await asyncio.sleep(time)
         await self.remove_mute(guild, member)    
 
-    async def add_mute(self, ctx, member: discord.User, info: MuteInfo) -> None:        
-        mute_role = await self.roles.get_mute_role(ctx.guild)
-
-        if mute_role in member.roles:
-            return await ctx.answer(ctx.lang["moderation"]["already_muted"].format(
-                member.mention))
-
-        reason = ctx.lang["moderation"]["add_mute"].format(
-            str(ctx.author), info.reason)
-
-        await member.add_roles(mute_role, reason=reason)
-
-        task = self.loop.create_task(self.mute_task(ctx.guild, member, info.time))
-        self.pool[self._create_pair(ctx.guild, member)] = task
-
-    async def remove_mute(self, ctx, member: discord.User) -> None:
+    async def add_mute(self, ctx, member: discord.User, info: MuteInfo) ->:        
         mute_role = await self.roles.get_mute_role(guild)
 
-        if mute_role not in member.roles:
-            return await ctx.answer(ctx.lang["moderation"]["not_muted"].format(
-                member.mention))
+        await member.add_roles(mute_role, reason=info.reason)
 
-        try:
-            await member.remove_roles(
-                mute_role,
-                reason=ctx.lang["moderation"]["mute_removal"])
-        except:
-            pass
+        if info.time > 0:
+            task = self.loop.create_task(
+                self.mute_task(guild, member, info.time.passed_seconds()))
+            self.pool[self._create_pair(guild, member)] = task
+
+    async def remove_mute(self, guild, member: discord.User, info: MuteInfo):
+        mute_role = await self.roles.get_mute_role(guild)
+
+        await member.remove_roles(mute_role, reason=info.reason)
 
         pair = self._create_pair(guild, member)
 
@@ -106,13 +115,10 @@ class ModerationCog(commands.Cog):
         self.mute_pool = MutePool(bot.loop, self.roles)
 
     async def log_entry(self, ctx, entry_type: EntryType, member: discord.Member, info: MuteInfo):
-        time = time or UnixTime.now()
-        reason = reason or ctx.lang["moderation"]["no_reason"]
-        
         await self.bot.db.execute("INSERT INTO `mod_log` VALUES (?, ?, ?, ?, ?, ?)",
             ctx.guild.id, ctx.author.id, 
             member.id, entry_type.name, 
-            time.timestamp, reason, 
+            info.time.timestamp, info.reason, 
             with_commit=True)
 
     # TODO:
@@ -121,15 +127,25 @@ class ModerationCog(commands.Cog):
 
     @commands.command()
     @is_moderator(kick_members=True)
-    async def mute(self, ctx, member: discord.Member, time: Optional[HumanTime]=None, *, reason: str=None):
+    async def mute(self, ctx, member: discord.Member, time: Optional[int]=None, *, reason: str=None):
         if self.mute_pool.is_muted(ctx.guild, member):
             return await ctx.answer(ctx.lang["moderation"]["already_muted"].format(
                 member.mention))
 
-        info = MuteInfo(time=time, reason=reason)
+        info = MuteInfo(
+            time=time or UnixTime.now(), 
+            reason=reason or ctx.lang["moderation"]["no_reason"])
 
         await self.mute_pool.add_mute(ctx, member, info) 
+
+        await ctx.answer(ctx.lang["moderation"]["muted"].format(
+            member.mention))
+
         await self.log_entry(ctx, EntryType.Mute, member, info)
+            
+    async def unmute(self, *args):
+        #await ctx.answer(ctx.lang["moderation"]["unmuted"].format(
+        #    member.mention))
 
 
 def setup(bot):
