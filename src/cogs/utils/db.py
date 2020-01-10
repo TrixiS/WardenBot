e73ddb4ab26c1bbabdc477475c61a558c49fca21
TrixiS <oficialmorozov@gmail.com>
2020-01-10 16:47:44 +0500

    Refactored db methods, added executemany

--- src/cogs/utils/db.py
+++ src/cogs/utils/db.py
@@ -41,85 +41,69 @@ class Db:
             self.conn.create_function("rand", 0, random)
             self.conn.create_function("unix_timestamp", 0, time)
 
+    def _wrap_args(self, seq):
+        for row in seq:
+            yield (row, )
+
     async def commit(self) -> None:
         async with self._lock:
             self.conn.commit()
 
-    async def execute(self, query: str, *args: Collection, fetch_all=False, with_commit=False) -> Optional[Any]:
-        try:
-            if self.db_type is DbType.MySQL:
-                self.cursor.execute(self.prepare_query(query, list(args)))
-            elif self.db_type is DbType.SQLite:
-                self.cursor.execute(query, args)
-        except Exception as e:
-            logging.warn(f"{str(e)} ({query}) ({args})")
-            return None
+    async def execute(self, query: str, *args, **kwargs):
+        if self.db_type is DbType.MySQL:
+            query = self.prepare_query(query)
 
+        try:
+            self.cursor.execute(query, args)
+        except Exception as e:
+            logging.error(f"{str(e)} ({query}) ({args})")
+            return
+
+        return await self.fetch(**kwargs)
+
+    async def executemany(self, query: str, args, **kwargs):
+        if self.db_type is DbType.MySQL:
+            query = self.prepare_query(query)
+
+        if kwargs.pop("wrap_args", False):
+            args = self._wrap_args(args)
+
+        try:
+            self.cursor.executemany(query, args)
+        except Exception as e:
+            logging.error(f"{str(e)} ({query}) ({args})")
+            return
+
+        return await self.fetch(**kwargs)
+
+    async def fetch(self, *, fetch_all=False, with_commit=False):
         if with_commit:
             count = self.cursor.rowcount
-            
+
             if count:
                 await self.commit()
 
             return count
-
 
         if fetch_all:
             return self.cursor.fetchall()
 
         fetched = self.cursor.fetchone()
 
-        if fetched is not None and len(fetched) == 1:
-            return fetched[0]
+        return fetched[0] if fetched is not None and len(fetched) == 1 \
+            else fetched
 
-        return fetched
+    def prepare_query(self, query: str) -> str:
+        result = ""
 
-    def prevent_injection(self, arg: Any) -> Any:
-        if isinstance(arg, int) or isinstance(arg, float):
-            if arg > self.int_max_bound:
-                arg = self.int_max_bound
-            elif arg < self.int_min_bound:
-                arg = self.int_min_bound
-
-            arg = str(arg)
-        elif isinstance(arg, str):
-            if len(arg) == 0:
-                return self.null
-
-            new_arg = ""
-            
-            for s in arg:
-                if s in "'\"":
-                    new_arg += ('\\' + s)
-                else:
-                    new_arg += s
-
-            arg = f"'{new_arg}'"
-        elif isinstance(arg, bool):
-            arg = str(int(arg))
-        else:
-            arg = self.null
-
-        return arg
-
-    def prepare_query(self, query: str, to_substit: list) -> str:
-        params_count = query.count(self.param_lit)
-        args_len = len(to_substit)
-
-        if params_count != args_len:
-            raise Exception(f"Params count must be equal to args length ({params_count}:{args_len}) ({query})")
-
-        if params_count == 0:
-            return query
-
-        sql = ""
+        in_param = False
 
         for s in query:
-            if s == self.param_lit:
-                arg = to_substit.pop(0)
+            if s in "`'\"":
+                in_param = not in_param
+            elif s == self.param_lit:
+                s = r"%s"
 
-                sql += self.prevent_injection(arg)
-            else:
-                sql += s
-                
-        return sql
+            result += s
+
+        return result
