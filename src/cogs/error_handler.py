cc1720f64f62b65fae4967a321a814639928c6ee
TrixiS <oficialmorozov@gmail.com>
2020-02-05 21:09:12 +0500

    Small handler improvements

--- src/cogs/error_handler.py
+++ src/cogs/error_handler.py
@@ -2,6 +2,7 @@ import discord
 import logging
 import traceback
 import sys
+import traceback
 
 from discord.ext import commands
 
@@ -19,20 +20,22 @@ class ErrorHandler(commands.Cog):
     # TODO: add lang support for d.py exceptions
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if hasattr(error, "message"):
-            message = error.message
+        if not isinstance(error, commands.CommandError):
+            message = getattr(error, "message", str(error))
+
+            em = discord.Embed(colour=ctx.color)
+
+            em.add_field(
+                name=ctx.lang["error_handler"]["title"].format(ctx.command.qualified_name),
+                value=message)
+
+            em.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+
+            await ctx.send(embed=em)
         else:
-            message = str(error)
-
-        em = discord.Embed(colour=ctx.color)
-
-        em.add_field(
-            name=ctx.lang["error_handler"]["title"].format(ctx.command.qualified_name),
-            value=message)
-
-        em.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-
-        await ctx.send(embed=em)
+            await ctx.answer(ctx.lang["errors"]["unknown_error"])
+            fmt = traceback.format_exception(type(error), error, error.__traceback__)
+            logging.error(''.join(fmt))
 
 
 def setup(bot):
