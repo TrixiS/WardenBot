994dd7b121ac5ecb6f3e6f256318c67b208ff92d
TrixiS <oficialmorozov@gmail.com>
2020-02-14 16:29:11 +0500

    Add tcp connector to plugin loader

--- src/plugin_loader/Program.cs
+++ src/plugin_loader/Program.cs
@@ -1,4 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+ // TODO:
+//    make one solution and repo for all plugins
+//    one plugin -> one project
 
 namespace PluginLoader
 {
@@ -10,6 +16,11 @@ namespace PluginLoader
 
             pluginLoader.RunPluginsFromPath(args[0]);
 
+            var connector = new WardenConnector("127.0.0.1", 6969, args[1]);
+
+            connector.OnCommandReceived += (sender, eventArgs) => Debug.WriteLine("Received command");
+            connector.Start();
+            
             await Task.Delay(-1);
         }
     }
---
+++ src/plugin_loader/WardenConnector.cs
@@ -0,0 +1,107 @@
+﻿#pragma warning disable CS4014
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace PluginLoader
+{
+    internal class ConnectorEventArgs : EventArgs
+    {
+        internal string Command { get; set; }
+        internal string[] Args { get; set; }
+        internal Socket Socket { get; set; }
+    }
+
+    internal class WardenConnector
+    {
+        private const int KeyBufferSize = 1024;
+        private const int CommandBufferSize = KeyBufferSize * 10;
+
+        private readonly string ownerKey;
+        private readonly TcpListener listener;
+        private readonly CancellationTokenSource tokenSource;
+        private readonly BufferStringDecoder stringDecoder;
+        
+        internal event EventHandler<ConnectorEventArgs> OnCommandReceived;
+
+        internal WardenConnector(string ip, int port, string ownerKey)
+        {
+            this.listener = new TcpListener(IPAddress.Parse(ip), port);
+            this.tokenSource = new CancellationTokenSource();
+            this.stringDecoder = new BufferStringDecoder();
+            this.ownerKey = ownerKey;
+        }
+
+        internal void Start()
+        {
+            this.listener.Start();
+            Task.Run(async () => await this.ListenAsync(this.tokenSource.Token));
+        }
+
+        internal void Stop()
+        {
+            this.tokenSource.Cancel();
+            this.listener.Stop();
+        }
+        
+        private async Task ListenAsync(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    return;
+                
+                Socket socket = await this.listener.AcceptSocketAsync();
+                Task.Run(() => this.Operate(socket), cancellationToken);
+            }
+        }
+
+        private void Operate(Socket socket)
+        {
+            while (true)
+            {
+                string ReceiveString (int bufferSize)
+                {
+                    byte[] data = new byte[bufferSize];
+                    int bytesCount = socket.Receive(data);
+                    return this.stringDecoder.DecodeString(bytesCount, data);
+                }
+            
+                string key = ReceiveString(KeyBufferSize);
+
+                // TODO: make rich key system
+                if (this.ownerKey != key)
+                {
+                    socket.Close();
+                    return;
+                }
+
+                var json = JObject.Parse(ReceiveString(CommandBufferSize));
+
+                this.OnCommandReceived?.Invoke(null, new ConnectorEventArgs
+                {
+                    Socket = socket,
+                    Command = (string) json["Command"],
+                    Args = json["Args"].ToObject<string[]>()
+                });
+            }
+        }
+    }
+
+    class BufferStringDecoder
+    {
+        private static Decoder StringDecoder = Encoding.UTF8.GetDecoder();
+
+        public string DecodeString(int bytesCount, byte[] buffer)
+        {
+            char[] chars = new char[bytesCount];
+            StringDecoder.GetChars(buffer, 0, bytesCount, chars, 0);
+            return new string(chars);
+        }
+    }
+}
