383c80b0b5e5421ec8179d0644f3e79362e5cfff
TrixiS <oficialmorozov@gmail.com>
2020-02-15 18:43:06 +0500

    Move load logic to a single class

--- src/plugin_loader.py
+++ src/plugin_loader.py
@@ -1,19 +1,27 @@
 import socket
 import json
+import subprocess
 
 from enum import Enum
+
 
 class Commands(Enum):
 
     load_plugin = 0
 
 
-class PluginLoaderConnector:
+class PluginLoader:
 
     def __init__(self, bot):
         self.bot = bot
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect(self.bot.config.plugin_loader_address)
+        self.process = subprocess.Popen(f"dotnet {self.config.csharp_plugins_loader}" \
+            f" {str(bot.assets_path / 'plugins')}" \
+            f" {self.config.plugin_loader_owner_key}")
+
+    def __del__(self):
+        self.process.terminate()
 
     async def send_command(self, command, *args):
         self.socket.send(
